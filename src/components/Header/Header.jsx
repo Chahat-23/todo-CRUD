a87aca6ce64796e66a34508e33178d319fb3d8c9
Chahat-23 <chahatshrivastava230620@gmail.com>
2024-09-07 20:21:53 +0530

    TODO-List-App-Done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <div className="header">
-      <img className='list-logo' src="/public/images/file.png" alt="file" />
+      <img className='list-logo' src="/public/images/file.png" />
       <h1>TODO</h1>
     </div>
   );
